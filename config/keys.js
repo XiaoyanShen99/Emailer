193c47b28693a9047b263fae675bffeb4d53f4ba
Xiaoyan Shen <shenxy1999@gmail.com>
2023-07-19 16:06:04 -0400

    ignore update

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    googleClientID: '250110929586-i2ohd5luefq3qla2qhqdt9f49upjo9np.apps.googleusercontent.com',
-    googleClientSecret: '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',
-    mongoURI: 'mongodb+srv://shenxy1999:BUdcUot9Hy4GCC7p@emailer-cluster.jziyaab.mongodb.net/emailer-cluster?retryWrites=true&w=majority',
-    cookieKey: 'sghuewnt4675890qnjksagh80qhtuoi423nnzhhtuiewok3,;',
-}
-
