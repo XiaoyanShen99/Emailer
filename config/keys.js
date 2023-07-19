8ad780c6a4b5714ca05bab69203699669818307d
Xiaoyan Shen <shenxy1999@gmail.com>
2023-07-19 10:37:26 -0400

    INIT + Google oauth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    googleClientID: '250110929586-i2ohd5luefq3qla2qhqdt9f49upjo9np.apps.googleusercontent.com',
+    googleClientSecret: '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'
+}
+
