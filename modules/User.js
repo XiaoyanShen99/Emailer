eb8faa284066bd3fcd63365baa47a72753d8a115
Xiaoyan Shen <shenxy1999@gmail.com>
2023-07-28 23:10:23 -0400

    payment setup

--- modules/User.js
+++ modules/User.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    googleId: String
+    googleId: String,
+    credits: { type:Number, default:0 },
 });
 
 // tell mongoose to create/update the schema. 'user' is the name
